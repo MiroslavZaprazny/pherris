cceb9729603d8518a65775cc74986d85f8751ddc
MiroslavZaprazny <miro.zaprazny8@gmail.com>
2024-12-22 18:56:43 +0100

    get current node under cursor

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
+use dashmap::DashMap;
 use tower_lsp::jsonrpc::Result;
 use tower_lsp::lsp_types::*;
 use tower_lsp::{Client, LanguageServer, LspService, Server};
+use tracing::debug;
 use tracing::level_filters::LevelFilter;
 use tracing_subscriber::fmt::Subscriber;
-use tracing::debug;
-use tree_sitter::Tree;
-use dashmap::DashMap;
+use tree_sitter::{Node, Point, Tree};
 
 #[derive(Debug)]
 struct Backend {
     client: Client,
-    ast_map: DashMap<Url, Tree>
+    ast_map: DashMap<Url, Tree>,
 }
 
 #[tower_lsp::async_trait]
@@ -19,15 +19,15 @@ impl LanguageServer for Backend {
         Ok(InitializeResult {
             capabilities: ServerCapabilities {
                 text_document_sync: Some(TextDocumentSyncCapability::Options(
-                    TextDocumentSyncOptions{
+                    TextDocumentSyncOptions {
                         open_close: Some(true),
-                        change: Some(TextDocumentSyncKind::FULL), // dont we want incremental
+                        change: Some(TextDocumentSyncKind::FULL), // we probably want incremental?
                         save: Some(TextDocumentSyncSaveOptions::SaveOptions(SaveOptions {
                             include_text: Some(true),
                         })),
-                        will_save: Some(true), //idk what this does
-                        will_save_wait_until: Some(true) //idk what this does
-                    }
+                        will_save: Some(true),            //idk what this does
+                        will_save_wait_until: Some(true), //idk what this does
+                    },
                 )),
                 definition_provider: Some(OneOf::Left(true)),
                 ..Default::default()
@@ -47,32 +47,44 @@ impl LanguageServer for Backend {
         params: GotoDefinitionParams,
     ) -> Result<Option<GotoDefinitionResponse>> {
         debug!("Goto definition params: {:?}", params);
-        let url = Url::parse("/Users/miroslavzaprazny/workspace/services").expect("to parse url");
-        let range = Range::new(Position::new(0, 0), Position::new(0,0));
-        let location = Location::new(url, range);
+
+        let tree = self
+            .ast_map
+            .get(&params.text_document_position_params.text_document.uri)
+            .expect("to get the tree");
+        let current_point = get_point_from_position(&params.text_document_position_params.position);
+        let current_node = get_node_for_point(&tree, current_point).expect("to get node");
+        print_node(current_node, 0);
+
+        let range = Range::new(
+            params.text_document_position_params.position,
+            params.text_document_position_params.position,
+        );
+        let location = Location::new(
+            params.text_document_position_params.text_document.uri,
+            range,
+        );
         let response = GotoDefinitionResponse::Scalar(location);
 
         Ok(Some(response))
     }
 
-    /// The [`textDocument/didOpen`] notification is sent from the client to the server to signal
-    /// that a new text document has been opened by the client.
-    ///
-    /// [`textDocument/didOpen`]: https://microsoft.github.io/language-server-protocol/specification#textDocument_didOpen
-    ///
-    /// The document's truth is now managed by the client and the server must not try to read the
-    /// document’s truth using the document's URI. "Open" in this sense means it is managed by the
-    /// client. It doesn't necessarily mean that its content is presented in an editor.
     async fn did_open(&self, params: DidOpenTextDocumentParams) {
         debug!("Got a textDocument/didOpen notification");
+        if self.ast_map.contains_key(&params.text_document.uri) {
+            return;
+        }
 
         let lang = tree_sitter_php::LANGUAGE_PHP;
         let mut parser = tree_sitter::Parser::new();
         parser.set_language(&lang.into()).expect("to set lang");
 
         //should we panic?
-        let tree = parser.parse(params.text_document.text, None).expect("to parse file");
-        self.ast_map.insert(params.text_document.uri, tree).expect("to insert tree");
+        let tree = parser
+            .parse(params.text_document.text, None)
+            .expect("to parse file");
+        print_tree(&tree);
+        self.ast_map.insert(params.text_document.uri, tree);
     }
 
     async fn shutdown(&self) -> Result<()> {
@@ -80,9 +92,48 @@ impl LanguageServer for Backend {
     }
 }
 
+fn get_point_from_position(position: &Position) -> Point {
+    Point {
+        row: position.line as usize,
+        column: position.character as usize,
+    }
+}
+
+fn get_node_for_point(tree: &Tree, point: Point) -> Option<Node> {
+    return tree.root_node().descendant_for_point_range(point, point);
+}
+
+fn print_tree(tree: &Tree) {
+    let root_node = tree.root_node();
+
+    print_node(root_node, 0);
+}
+
+fn print_node(node: Node, depth: usize) {
+    let indent = "  ".repeat(depth);
+
+    debug!(
+        "{}{} [{}, {}] - [{}, {}]",
+        indent,
+        node.kind(),
+        node.start_position().row,
+        node.start_position().column,
+        node.end_position().row,
+        node.end_position().column
+    );
+
+    let mut cursor = node.walk();
+    for child in node.children(&mut cursor) {
+        print_node(child, depth + 1);
+    }
+}
+
 #[tokio::main]
 async fn main() {
-    let appender = tracing_appender::rolling::never("/Users/miroslavzaprazny/personal/pherris/debug/", "log.txt");
+    let appender = tracing_appender::rolling::never(
+        "/Users/miroslavzaprazny/personal/pherris/debug/",
+        "log.txt",
+    );
     let (writer, _guard) = tracing_appender::non_blocking(appender);
     let subscriber = Subscriber::builder()
         .with_writer(writer)
@@ -93,6 +144,9 @@ async fn main() {
     debug!("Starting lsp server");
     let stdin = tokio::io::stdin();
     let stdout = tokio::io::stdout();
-    let (service, socket) = LspService::new(|client| Backend { client, ast_map: DashMap::default() });
+    let (service, socket) = LspService::new(|client| Backend {
+        client,
+        ast_map: DashMap::default(),
+    });
     Server::new(stdin, stdout, socket).serve(service).await;
 }
