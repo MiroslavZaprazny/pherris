bb6f92ca8f6e8e521e701a66e9e0c55ecb8bb9ad
MiroslavZaprazny <miro.zaprazny8@gmail.com>
2024-12-23 17:43:41 +0100

    tidy up a bit

---
+++ src/lsp/lsp.rs
@@ -0,0 +1,157 @@
+use crate::tree::utils::{
+    get_node_for_point, get_point_from_position, get_position_from_point, print_tree,
+};
+use dashmap::DashMap;
+use streaming_iterator::StreamingIterator;
+use tower_lsp::jsonrpc::Result;
+use tower_lsp::lsp_types::*;
+use tower_lsp::{Client, LanguageServer};
+use tracing::debug;
+use tree_sitter::{Query, QueryCursor, Tree};
+
+#[derive(Debug)]
+pub struct Backend {
+    pub client: Client,
+    pub ast_map: DashMap<Url, Tree>,
+    pub document_map: DashMap<Url, String>,
+}
+
+#[tower_lsp::async_trait]
+impl LanguageServer for Backend {
+    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+        Ok(InitializeResult {
+            capabilities: ServerCapabilities {
+                text_document_sync: Some(TextDocumentSyncCapability::Options(
+                    TextDocumentSyncOptions {
+                        open_close: Some(true),
+                        change: Some(TextDocumentSyncKind::FULL), // we probably want incremental?
+                        save: Some(TextDocumentSyncSaveOptions::SaveOptions(SaveOptions {
+                            include_text: Some(true),
+                        })),
+                        will_save: Some(true),            //idk what this does
+                        will_save_wait_until: Some(true), //idk what this does
+                    },
+                )),
+                definition_provider: Some(OneOf::Left(true)),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+    }
+
+    async fn initialized(&self, _: InitializedParams) {
+        self.client
+            .log_message(MessageType::INFO, "server initialized!")
+            .await;
+    }
+
+    async fn goto_definition(
+        &self,
+        params: GotoDefinitionParams,
+    ) -> Result<Option<GotoDefinitionResponse>> {
+        debug!("Goto definition params: {:?}", params);
+
+        let tree = self
+            .ast_map
+            .get(&params.text_document_position_params.text_document.uri)
+            .expect("to get the tree");
+
+        let document = self
+            .document_map
+            .get(&params.text_document_position_params.text_document.uri)
+            .expect("to get the document");
+
+        let current_point = get_point_from_position(&params.text_document_position_params.position);
+        let current_node = get_node_for_point(&tree, current_point).expect("to get node");
+
+        match current_node.kind() {
+            "name" => {
+                let location = self
+                    .find_variable_declaration(
+                        &current_node,
+                        &document,
+                        &params.text_document_position_params.text_document.uri,
+                        &tree,
+                    )
+                    .expect("to find variable declaration");
+
+                return Ok(Some(GotoDefinitionResponse::Scalar(location)));
+            }
+            _ => return Ok(None),
+        }
+    }
+
+    async fn did_open(&self, params: DidOpenTextDocumentParams) {
+        debug!("Got a textDocument/didOpen notification");
+        if self.ast_map.contains_key(&params.text_document.uri) {
+            return;
+        }
+
+        let lang = tree_sitter_php::LANGUAGE_PHP;
+        let mut parser = tree_sitter::Parser::new();
+        parser.set_language(&lang.into()).expect("to set lang");
+
+        //should we panic?
+        let tree = parser
+            .parse(params.text_document.text.clone(), None)
+            .expect("to parse file");
+        print_tree(&tree);
+
+        self.ast_map.insert(params.text_document.uri.clone(), tree);
+        self.document_map
+            .insert(params.text_document.uri, params.text_document.text);
+    }
+
+    async fn shutdown(&self) -> Result<()> {
+        Ok(())
+    }
+}
+
+impl Backend {
+    fn find_variable_declaration(
+        &self,
+        current_node: &tree_sitter::Node,
+        document: &str,
+        uri: &Url,
+        tree: &tree_sitter::Tree,
+    ) -> Option<Location> {
+        // let var_query = Query::new(&tree_sitter_php::LANGUAGE_PHP.into(),
+        //     "(name) @variable").ok()?;
+        let declare_query = Query::new(
+            &tree_sitter_php::LANGUAGE_PHP.into(),
+            "(assignment_expression left: (variable_name) @declaration)",
+        )
+        .ok()?;
+
+        // let mut var_cursor = QueryCursor::new();
+        let mut declare_cursor = QueryCursor::new();
+
+        // let mut var_matches = var_cursor.matches(&var_query, *current_node, document.as_bytes());
+        // while let Some(var_match) = var_matches.next() {
+        // for var_capture in var_match.captures {
+        let var_name = current_node.utf8_text(document.as_bytes()).ok()?;
+
+        let mut declare_matches =
+            declare_cursor.matches(&declare_query, tree.root_node(), document.as_bytes());
+        while let Some(declare_match) = declare_matches.next() {
+            for declare_capture in declare_match.captures {
+                let declare_var_name = declare_capture
+                    .node
+                    .utf8_text(document.as_bytes())
+                    .ok()?
+                    .trim_start_matches("$");
+
+                if declare_var_name == var_name {
+                    let range = Range::new(
+                        get_position_from_point(&declare_capture.node.start_position()),
+                        get_position_from_point(&declare_capture.node.end_position()),
+                    );
+                    return Some(Location::new(uri.clone(), range));
+                }
+            }
+        }
+        // }
+        // }
+        None
+    }
+}
---
+++ src/tree/utils.rs
@@ -0,0 +1,46 @@
+use tower_lsp::lsp_types::Position;
+use tracing::debug;
+use tree_sitter::{Node, Point, Tree};
+
+pub fn get_point_from_position(position: &Position) -> Point {
+    Point {
+        row: position.line as usize,
+        column: position.character as usize,
+    }
+}
+
+pub fn get_position_from_point(point: &Point) -> Position {
+    Position {
+        line: point.row as u32,
+        character: point.column as u32,
+    }
+}
+
+pub fn get_node_for_point(tree: &Tree, point: Point) -> Option<Node> {
+    return tree.root_node().descendant_for_point_range(point, point);
+}
+
+pub fn print_tree(tree: &Tree) {
+    let root_node = tree.root_node();
+
+    print_node(root_node, 0);
+}
+
+pub fn print_node(node: Node, depth: usize) {
+    let indent = "  ".repeat(depth);
+
+    debug!(
+        "{}{} [{}, {}] - [{}, {}]",
+        indent,
+        node.kind(),
+        node.start_position().row,
+        node.start_position().column,
+        node.end_position().row,
+        node.end_position().column
+    );
+
+    let mut cursor = node.walk();
+    for child in node.children(&mut cursor) {
+        print_node(child, depth + 1);
+    }
+}
