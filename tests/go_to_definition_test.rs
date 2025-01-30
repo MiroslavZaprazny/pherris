2b3ec03c57d12f301a01a66b09335750a61ba29f
MiroslavZaprazny <miro.zaprazny8@gmail.com>
2025-01-30 18:30:25 +0100

    more tests

--- tests/go_to_definition_test.rs
+++ tests/go_to_definition_test.rs
@@ -45,6 +45,80 @@ fn test_find_variable_decleration() {
     }
 }
 
+#[test]
+fn test_find_shadowed_variable_decleration() {
+    let file_contents = r#"<?php
+        $variable = 0;
+        $variable = 1;
+        echo $variable;
+    "#;
+    let temp_dir = TempDir::new().expect("to initialize temp dir");
+    let temp_dir_path = temp_dir.path();
+
+    let uri = Url::from_file_path(temp_dir_path).unwrap();
+    let mut parser = Parser::new().expect("to create a parser");
+    let tree = parser.parse(file_contents).expect("to parse file");
+
+    let ast_map = DashMap::new();
+    ast_map.insert(uri.clone(), tree);
+    let document_map = DashMap::new();
+    document_map.insert(uri.clone(), String::from(file_contents));
+
+    prepare_php_file(temp_dir_path, file_contents);
+    let state = State::new(
+        ast_map,
+        document_map,
+        RwLock::new(String::from(temp_dir_path.to_str().unwrap())),
+        DashMap::default(),
+    );
+
+    let response =
+        handle_go_to_definition(&uri, &Position::new(2, 15), &state, &RwLock::new(parser));
+    assert!(response.is_some());
+    if let GotoDefinitionResponse::Scalar(response) = response.unwrap() {
+        assert_eq!(response.range.start.line, 2);
+        assert_eq!(response.range.start.character, 8);
+    } else {
+        panic!("response is not a location");
+    }
+}
+
+#[test]
+fn test_find_variable_definition_in_array_function() {
+    let file_contents = r#"<?php
+        array_map(fn ($test) => $test, []);
+    "#;
+    let temp_dir = TempDir::new().expect("to initialize temp dir");
+    let temp_dir_path = temp_dir.path();
+
+    let uri = Url::from_file_path(temp_dir_path).unwrap();
+    let mut parser = Parser::new().expect("to create a parser");
+    let tree = parser.parse(file_contents).expect("to parse file");
+
+    let ast_map = DashMap::new();
+    ast_map.insert(uri.clone(), tree);
+    let document_map = DashMap::new();
+    document_map.insert(uri.clone(), String::from(file_contents));
+
+    prepare_php_file(temp_dir_path, file_contents);
+    let state = State::new(
+        ast_map,
+        document_map,
+        RwLock::new(String::from(temp_dir_path.to_str().unwrap())),
+        DashMap::default(),
+    );
+
+    let response =
+        handle_go_to_definition(&uri, &Position::new(2, 15), &state, &RwLock::new(parser));
+    assert!(response.is_some());
+    if let GotoDefinitionResponse::Scalar(response) = response.unwrap() {
+        assert_eq!(response.range.start.line, 2);
+        assert_eq!(response.range.start.character, 22);
+    } else {
+        panic!("response is not a location");
+    }
+}
+
 fn prepare_php_file(root: &Path, file_contents: &str) {
     let file_path = format!("{}/test_file.php", root.to_str().unwrap());
 
