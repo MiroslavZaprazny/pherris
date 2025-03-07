2b8177a87a048737efa9cc4fcc69f606574ba6bb
MiroslavZaprazny <miro.zaprazny8@gmail.com>
2025-03-07 22:44:37 +0100

    add test

--- tests/go_to_definition_test.rs
+++ tests/go_to_definition_test.rs
@@ -319,6 +319,63 @@ fn test_find_class_definition_in_same_folder() {
 }
 
 #[test]
+fn test_find_class_definition_in_same_folder_using_class_name_as_filename() {
+    let file_contents = r#"<?php
+        function (MyClass $test) {
+            echo $test;
+        }
+    "#;
+    let temp_dir = TempDir::new().expect("to initialize temp dir");
+    let path_str = format!("{}/{}", temp_dir.path().to_str().unwrap(), "test.php");
+    let temp_dir_path = Path::new(&path_str);
+    let target_uri = Url::from_file_path(temp_dir_path).unwrap();
+
+    let mut parser = Parser::new().expect("to create a parser");
+    let tree = parser.parse(file_contents).expect("to parse file");
+
+    let ast_map = DashMap::new();
+    ast_map.insert(target_uri.clone(), tree);
+    let document_map = DashMap::new();
+    document_map.insert(target_uri.clone(), String::from(file_contents));
+
+    let state = State::new(
+        ast_map,
+        document_map,
+        RwLock::new(String::from(temp_dir_path.to_str().unwrap())),
+        DashMap::default(),
+    );
+
+    prepare_php_file(temp_dir_path, file_contents);
+
+    let file_contents = r#"<?php
+        namespace MyNamespace;
+
+        class MyClass {}
+    "#;
+
+    let path_str = format!("{}/{}", temp_dir.path().to_str().unwrap(), "MyClass.php");
+    let temp_dir_path = Path::new(&path_str);
+    let class_uri = Url::from_file_path(temp_dir_path).unwrap();
+
+    prepare_php_file(temp_dir_path, file_contents);
+
+    let response = handle_go_to_definition(
+        &target_uri,
+        &Position::new(1, 19),
+        &state,
+        &RwLock::new(parser),
+    );
+    assert!(response.is_some());
+    if let GotoDefinitionResponse::Scalar(response) = response.unwrap() {
+        assert_eq!(response.uri.as_str(), class_uri.as_str());
+        assert_eq!(response.range.start.line, 3);
+        assert_eq!(response.range.start.character, 14);
+    } else {
+        panic!("response is not a location");
+    }
+}
+
+#[test]
 fn test_find_class_definition_in_different_folder() {
     let file_contents = r#"<?php
         use MyApp\Testing\MyClass;
